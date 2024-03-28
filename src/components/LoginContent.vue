9269609957f4e117d07fb292e879e8930a847b38
Pradeep Dilip Mokashi <94032066+PolyMokashi@users.noreply.github.com>
2024-03-28 13:01:55 +0530

    Update LoginContent.vue

--- src/components/LoginContent.vue
+++ src/components/LoginContent.vue
@@ -45,7 +45,7 @@
 
 <script>
 // import axios from 'axios';
-import { required, max, regex } from "vee-validate/dist/rules";
+import { required,digits, email, max, regex } from "vee-validate/dist/rules";
 import {
   extend,
   ValidationObserver,
